40a0e648f2f67630e9acfbf6c64c52ef61a1ae24
root <chaley@hawk.iit.edu>
2020-03-21 23:48:04 -0500

    Remove manual react calls to prepare for auto-react system

--- Commands/theory.js
+++ Commands/theory.js
@@ -1,19 +1,19 @@
 module.exports = {
-    execute: function (msg) {
-        let theory = msg.removeCommand(msg.content)
-        let perms = msg.guild.channels.get(chans.theorylist).permissionsFor(msg.member)
+    execute: async function (msg) {
+        let theory = msg.removeCommand(msg.content);
+        let perms = msg.guild.channels.get(chans.theorylist).permissionsFor(msg.member);
         if (perms.has('EMBED_LINKS')) {
-            let embed = new Discord.RichEmbed({ description: theory })
-            embed.setFooter(msg.createdAt.toString() + ' || Submit a theory with !theory in the commands channel', bot.user.displayAvatarURL)
-            embed.setColor('#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6))
-            embed.setAuthor(msg.member.displayName)
-            embed.setThumbnail(msg.author.displayAvatarURL)
+            let embed = new Discord.RichEmbed({ description: theory });
+            embed.setFooter(msg.createdAt.toString() + ' || Submit a theory with !theory in the commands channel', bot.user.displayAvatarURL);
+            embed.setColor("RANDOM");
+            embed.setAuthor(msg.member.displayName);
+            embed.setThumbnail(msg.author.displayAvatarURL);
 
-            msg.guild.channels.get(chans.theorylist).send({ embed: embed }).then(async m => {await m.react('%E2%AC%86'); await m.react('%E2%AC%87')})
-            msg.channel.embed(`Post to <#${chans.theorylist}> successful!`)
-            msg.delete()
+            await msg.guild.channels.get(chans.theorylist).send({ embed: embed });
+            await msg.channel.embed(`Post to <#${chans.theorylist}> successful!`);
+            await msg.delete();
         } else {
-            msg.channel.embed(`You do not have permission to post to <#${chans.theorylist}>`)
+            await msg.channel.embed(`You do not have permission to post to <#${chans.theorylist}>`);
         }
 
     },

4721ac92460b1efb3e148a4a41d36f1676748940
Cameron Haley <chaley@hawk.iit.edu>
2021-06-05 12:47:39 -0500

    Add delete and suggest commands

---
+++ src/slashcommands/staff/delete.ts
@@ -0,0 +1,46 @@
+import { CommandError, CommandOptions, CommandRunner } from "configuration/definitions";
+import { MessageEmbed, Snowflake } from "discord.js";
+import { MessageTools } from "helpers";
+import F from "helpers/funcs";
+import { CommandOptionType } from "slash-create";
+
+export const Options: CommandOptions = {
+    description: "Deletes a certain number of messages",
+    options: [
+        {
+            name: "amount",
+            description: "The number of messages to delete",
+            required: true,
+            type: CommandOptionType.INTEGER
+        }
+    ]
+};
+
+export const Executor: CommandRunner<{
+    amount: number;
+}> = async (ctx) => {
+    const { amount } = ctx.opts;
+    const channel = ctx.channel;
+
+    if (amount > 500) throw new CommandError("You can only delete up to 100 messages at a time");
+
+    const deleted = await channel.bulkDelete(amount, true);
+
+    let numDeleted = deleted.size;
+
+    // Need to manually fetch and delete remaining messages
+    const remaining = amount - numDeleted;
+    console.log(`${remaining} remaining`);
+    if (remaining > 0) {
+        const fetched = await channel.messages.fetch({ limit: Math.min(remaining, 100) });
+
+        for (const m of fetched.array()) {
+            await m.delete();
+            await F.wait(750);
+        }
+        numDeleted += fetched.size;
+    }
+
+    const embed = new MessageEmbed({ description: `Deleted ${numDeleted} out of the requested ${amount} messages` });
+    await ctx.send({ embeds: [embed.toJSON()] });
+};
---
+++ src/slashcommands/submit/suggestion.ts
@@ -0,0 +1,38 @@
+import { channelIDs } from "configuration/config";
+import { CommandComponentListener, CommandOptions, CommandRunner } from "configuration/definitions";
+import { Poll } from "database/entities/Poll";
+import Discord, { MessageActionRow, MessageButton, MessageEmbed, TextChannel } from "discord.js";
+import { generateUpvoteDownvoteListener } from "helpers";
+import F from "helpers/funcs";
+import { last } from "ramda";
+import { CommandOptionType, ComponentActionRow } from "slash-create";
+
+export const Options: CommandOptions = {
+    description: "Submits a suggestion to the staff",
+    options: [
+        { name: "title", description: "The title of your suggestion", required: true, type: CommandOptionType.STRING },
+        {
+            name: "details",
+            description: "Some more details about your suggestion",
+            required: true,
+            type: CommandOptionType.STRING
+        }
+    ]
+};
+
+export const Executor: CommandRunner<{ title: string; details: string }> = async (ctx) => {
+    const { title, details } = ctx.opts;
+
+    const embed = new MessageEmbed()
+        .setAuthor(`Suggestion from ${ctx.member.displayName}`, ctx.member.user.displayAvatarURL())
+        .setColor(ctx.member.displayHexColor)
+        .setTitle(title)
+        .setDescription(details);
+
+    const suggestChan = ctx.member.guild.channels.cache.get(channelIDs.submittedsuggestions) as TextChannel;
+
+    await suggestChan.send({ embed });
+
+    const responseEmbed = new MessageEmbed({ description: "Your suggestion has been submitted!" });
+    await ctx.send({ embeds: [responseEmbed.toJSON()] });
+};

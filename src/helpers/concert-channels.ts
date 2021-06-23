05af5d307e21553449b39ddaa12f5a358539f4ae
Cameron Haley <chaley@hawk.iit.edu>
2021-06-23 17:44:17 -0500

    Add concert fetching, adding, deleting mechanisms

---
+++ src/helpers/concert-channels.ts
@@ -0,0 +1,194 @@
+import { categoryIDs, channelIDs, guildID, roles, userIDs } from "configuration/config";
+import { format } from "date-fns";
+import { CategoryChannel, Client, Guild, GuildChannel, OverwriteData, Role, TextChannel } from "discord.js";
+import fetch from "node-fetch";
+
+const CONCERT_URL = "https://rest.bandsintown.com/V3.1/artists/twenty%20one%20pilots/events/?app_id=js_127.0.0.1";
+const ROLE_HEX = "#1b5151";
+
+export interface Offer {
+    type: string;
+    url: string;
+    status: string;
+}
+
+export interface Venue {
+    country: string;
+    city: string;
+    latitude: string;
+    name: string;
+    location: string;
+    region: string;
+    longitude: string;
+}
+
+export interface ConcertEntry {
+    offers: Offer[];
+    venue: Venue;
+    datetime: Date;
+    artist: Record<string, unknown>;
+    on_sale_datetime: string;
+    description: string;
+    lineup: string[];
+    bandsintown_plus: boolean;
+    id: string;
+    title: string;
+    artist_id: string;
+    url: string;
+}
+
+class ConcertChannel {
+    // something-arena-chicago
+    public channelName: string;
+    public concerts: ConcertEntry[] = [];
+    constructor(public concert: ConcertEntry, private guild: Guild) {
+        const { venue, title } = concert;
+        const s = (str: string) => str.toLowerCase().replace(/\(.*?\)/g, "").normalize("NFKC").replace(/\p{Diacritic}/gu, "").split(/ +/); // prettier-ignore
+        this.channelName = [s(title || venue.name), s(venue.city)].flat().filter(a => a).join("-"); // prettier-ignore
+        this.concerts.push(concert);
+    }
+
+    addConcertsFrom(concert: ConcertChannel): boolean {
+        if (this.channelName !== concert.channelName) return false;
+        this.concerts.push(...concert.concerts);
+        return true;
+    }
+
+    get channelID() {
+        return this.guild.channels.cache.find((c) => c.name === this.channelName)?.id;
+    }
+}
+
+class ConcertChannelManager {
+    public concertChannels: ConcertChannel[] = [];
+    private concertCategory: CategoryChannel;
+    constructor(private guild: Guild) {
+        this.concertCategory = guild.channels.cache.get(categoryIDs.concerts) as CategoryChannel;
+    }
+
+    async fetchConcerts(): Promise<boolean> {
+        try {
+            const res = await fetch(CONCERT_URL);
+
+            const json = (await res.json()) as ConcertEntry[];
+            if (!json || !Array.isArray(json)) throw new Error("Not a valid array");
+
+            const chans = json.map((c) => new ConcertChannel(c, this.guild));
+            if (chans.length === 0) return false;
+
+            // Some concerts are multiple days, compress them into one channel to avoid confusion
+            const noDupes = [];
+            let lastChannel: ConcertChannel = chans[0];
+            for (let i = 1; i < chans.length - 1; i++) {
+                if (chans[i].channelName === lastChannel.channelName) {
+                    lastChannel.addConcertsFrom(chans[i]);
+                } else {
+                    noDupes.push(lastChannel);
+                    lastChannel = chans[i];
+                }
+            }
+            noDupes.push(lastChannel);
+
+            this.concertChannels = noDupes;
+
+            return true;
+        } catch (e) {
+            console.warn(`Failed to fetch concerts`, e);
+            return false;
+        }
+    }
+
+    async checkChannels(): Promise<boolean> {
+        try {
+            // this.concertChannels = [];
+            const channels = this.concertCategory.children.array();
+
+            // Channels in JSON list that don't have a channel
+            const toAdd = this.concertChannels.filter((c) => !channels.some((c2) => c.channelName === c2.name));
+
+            for (const t of toAdd) {
+                await this.#addChannel(t);
+            }
+
+            // Channels that exist, but are no longer in the JSON list
+            const toRemove = channels.filter((c) => !this.concertChannels.some((c2) => c2.channelName === c.name));
+
+            for (const c of toRemove) {
+                await this.#deleteChannel(c);
+            }
+
+            console.log(`[Concert Channels] Add ${toAdd.length} channels and removed ${toRemove.length} channels`);
+
+            return true;
+        } catch (e) {
+            console.warn(`Failed to check channels`, e);
+            return false;
+        }
+    }
+
+    async #addChannel(toAdd: ConcertChannel): Promise<void> {
+        const referenceRole = await this.guild.roles.fetch(roles.topfeed.divider);
+        if (!referenceRole) return;
+
+        const role = await this.guild.roles.create({
+            name: toAdd.channelName,
+            color: ROLE_HEX,
+            position: referenceRole.position + 1
+        });
+
+        const permissionOverwrites: OverwriteData[] = [
+            {
+                deny: ["VIEW_CHANNEL"],
+                id: guildID
+            },
+            {
+                allow: ["VIEW_CHANNEL", "SEND_MESSAGES"],
+                id: roles.staff // Staff
+            },
+            {
+                allow: ["VIEW_CHANNEL"],
+                id: role.id // The role that was just created
+            },
+            {
+                allow: ["VIEW_CHANNEL", "SEND_MESSAGES", "MANAGE_CHANNELS"],
+                id: roles.bots // Bots
+            },
+            {
+                deny: ["SEND_MESSAGES"],
+                id: roles.muted // Muted
+            }
+        ];
+
+        const dates = toAdd.concerts.map((c) => format(new Date(c.datetime), "d MMMM yyyy")).join(", ");
+        // prettier-ignore
+        const topic = `${dates} | Welcome to the ${toAdd.concert.title || toAdd.concert.venue.name} concert channel! Feel free to discuss the concert, tickets, share pictures, etc. This channel will be deleted 3 days after the concert ends.`
+
+        const channel = await this.guild.channels.create(toAdd.channelName, {
+            permissionOverwrites,
+            type: "text",
+            topic
+        });
+        await channel.setParent(categoryIDs.concerts, { lockPermissions: false });
+    }
+
+    async #deleteChannel(toDelete: GuildChannel): Promise<boolean> {
+        const roles = await toDelete.guild.roles.fetch();
+        const role = roles.find((r) => r.name === toDelete.name && r.hexColor === ROLE_HEX);
+
+        if (!role) {
+            const devChannel = toDelete.guild.channels.cache.get(channelIDs.bottest) as TextChannel;
+            await devChannel.send(
+                `<@${userIDs.me}> Unable to find associated role for ${toDelete} to delete. Not deleting.`
+            );
+            return false;
+        }
+
+        if (toDelete.parentID !== categoryIDs.concerts) return false;
+
+        await toDelete.delete();
+        await role.delete();
+        return true;
+    }
+}
+
+export default ConcertChannelManager;
---
+++ src/slashcommands/roles/concert.ts
@@ -0,0 +1,32 @@
+import { CommandOptions, CommandRunner } from "configuration/definitions";
+import { MessageEmbed, Role, Snowflake, TextChannel } from "discord.js";
+
+export const Options: CommandOptions = {
+    description: "Selects a concert channel",
+    options: [
+        /**
+         * These are automatically populated from concert-channels.ts concerts
+         */
+    ]
+};
+
+export const Executor: CommandRunner<Record<string, Snowflake>> = async (ctx) => {
+    await ctx.defer(true);
+    const channels = Object.values(ctx.opts).map((cid) => ctx.channel.guild.channels.cache.get(cid) as TextChannel);
+
+    const allRoles = await ctx.channel.guild.roles.fetch();
+    const rolesToGive = channels.map((c) => allRoles.find((r) => r.name === c.name)) as Role[];
+
+    const text = rolesToGive.map((r) => `${r}`).join(", ");
+    console.log({ channels, rolesToGive, text });
+
+    for (const role of rolesToGive) {
+        await ctx.member.roles.add(role);
+    }
+
+    const embed = new MessageEmbed()
+        .setTitle("Concert role(s) successfully added!")
+        .setDescription(`You got the following role(s): ${text}`);
+
+    await ctx.send({ embeds: [embed.toJSON()], allowedMentions: { roles: [], everyone: false } });
+};

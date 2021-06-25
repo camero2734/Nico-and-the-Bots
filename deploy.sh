d052386bd0214e62566e1b2a38856d5124d29e90
Cameron Haley <chaley@hawk.iit.edu>
2021-06-25 14:31:03 -0500

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 git switch heroku && # Switch to branch that has secret files
+git add -A && # Add any new files
 git merge main -s ours && # Ignore different .gitignore file
 git push -u heroku +HEAD:master # Push to heroku main branch
+
+git switch main

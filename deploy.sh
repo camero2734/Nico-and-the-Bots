87d7dd32d08de98f6dfb1d27409ab4cad353cc54
Cameron Haley <chaley@hawk.iit.edu>
2021-06-25 14:14:58 -0500

    Run script explicity with bash; Use && in script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-git switch heroku # Switch to branch that has secret files
-git merge main -s ours # Ignore different .gitignore file
+git switch heroku && # Switch to branch that has secret files
+git merge main -s ours && # Ignore different .gitignore file
 git push -u heroku +HEAD:master # Push to heroku main branch

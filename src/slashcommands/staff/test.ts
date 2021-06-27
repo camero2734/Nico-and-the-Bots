6609f96ea9d3c20805d2d36f49b1c94bbcfbc558
Cameron Haley <chaley@hawk.iit.edu>
2021-06-27 17:34:02 -0500

    Make Typescript happy again

--- src/slashcommands/staff/test.ts
+++ src/slashcommands/staff/test.ts
@@ -9,56 +9,6 @@ export const Options: CommandOptions = {
     options: []
 };
 
-const NUM_QUESTIONS = 15;
-
-function convFromString(utf16: string): boolean {
-    const charTo16Bits = (c: string) => c.charCodeAt(0).toString(2).padStart(16, "0");
-    const bits = utf16.split("").map(charTo16Bits);
-    const bitStr = bits.join("").slice(0, 4 * NUM_QUESTIONS); // Remove padding at end
-
-    console.log({ bitStr });
-    const answerIdxs = R.splitEvery(4, bitStr).map((bits) => parseInt(bits, 2));
-
-    for (let i = 0; i < answerIdxs.length; i++) {
-        const idx = answerIdxs[i];
-        console.log(idx);
-        // const question = this.questions[i];
-        // this.markAnswer(question, idx);
-    }
-
-    return true;
-}
-function convToString() {
-    let bitStr = "";
-    const questions = F.indexArray(NUM_QUESTIONS);
-    let i = 0;
-    for (const question of questions) {
-        const idx = i++;
-        const bits = idx.toString(2).padStart(4, "0");
-        bitStr += bits;
-    }
-
-    const padLength = 16 * Math.ceil(bitStr.length / 16);
-    const padded = bitStr.padEnd(padLength, "0");
-
-    const characters = R.splitEvery(16, padded)
-        .map((bits) => String.fromCharCode(parseInt(bits, 2)))
-        .join("");
-    console.log({ bitStr, padded });
-    return characters;
-}
-
 export const Executor: CommandRunner = async (ctx) => {
-    for (let i = 0; i < 10000; i++) {
-        const RAN_LENGTH = 28;
-        const bits = [...Array(RAN_LENGTH)].map((a) => (Math.random() < 0.5 ? "0" : "1")).join("");
-        const utf8 = F.bitStringToUTF8(bits);
-        const maybeBits = F.UTF8ToBitStr(utf8, RAN_LENGTH);
-
-        if (maybeBits !== bits) {
-            await ctx.send(`\`${bits}\` -> ${utf8} ${i}\n\n\`${maybeBits}\``);
-            return;
-        }
-    }
-    await ctx.send("Done");
+    //
 };

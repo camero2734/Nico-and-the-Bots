4c960ea92ef385b9e3d60d409452a99de3a08d52
Cameron Haley <chaley@hawk.iit.edu>
2021-07-17 17:08:55 -0500

    Finish house of gold

--- src/slashcommands/submit/gold.ts
+++ src/slashcommands/submit/gold.ts
@@ -9,44 +9,32 @@ import {
 import { Counter } from "database/entities/Counter";
 import { Economy } from "database/entities/Economy";
 import { Poll } from "database/entities/Poll";
-import { Message, MessageActionRow, MessageButton, MessageEmbed, Snowflake, TextChannel } from "discord.js";
+import {
+    DMChannel,
+    EmojiIdentifierResolvable,
+    GuildMember,
+    Message,
+    MessageActionRow,
+    MessageButton,
+    MessageEmbed,
+    TextChannel
+} from "discord.js";
 import { generateUpvoteDownvoteListener, MessageContext } from "helpers";
-import { CommandOptionType } from "slash-create";
 import { Connection } from "typeorm";
+import F from "../../helpers/funcs";
 
 export const Options: CommandOptions = {
-    description: `Gives a message gold. Must be used in #house-of-gold`,
-    options: [
-        {
-            name: "code",
-            description: "The code to submit a message to house of gold.",
-            required: true,
-            type: CommandOptionType.STRING
-        }
-    ]
+    description: `This command is a placeholder. Gold messages by reacting :gold: on them.`,
+    options: []
 };
 
 const answerListener = generateUpvoteDownvoteListener("goldResponse");
 export const ComponentListeners: CommandComponentListener[] = [answerListener];
 
-export const Executor: CommandRunner<{ code: string }> = async (ctx) => {
-    const hex = Buffer.from(ctx.opts.code, "base64").toString("hex");
-    const uniqueID = BigInt(`0x${hex}`).toString();
-    const [messageid, channelid] = [uniqueID.slice(0, 18), uniqueID.slice(18)] as Snowflake[];
-
-    const channel = ctx.channel.guild.channels.cache.get(channelid) as TextChannel;
-    if (!channel) throw new CommandError("Unable to find channel");
-
-    const msg = await channel.messages.fetch(messageid);
-    if (!msg) throw new CommandError("Unable to locate message");
-
-    const embed = new MessageEmbed()
-        .setTitle("Would you like to gold this mesage?")
-        .setAuthor(<string>msg.member?.displayName, msg.author.displayAvatarURL())
-        .setDescription(msg.content)
-        .setColor("#FCE300");
-
-    await ctx.send({ embeds: [embed.toJSON()] });
+export const Executor: CommandRunner = async () => {
+    throw new CommandError(
+        "This command is not usable. You can gold messages by reacting with <:gold:389216023141941249>."
+    );
 };
 
 // Give command to copy and paste if someone reacts with :gold:
@@ -55,7 +43,7 @@ export const ReactionHandler: CommandReactionHandler = async ({ reaction, user, 
 
     const member = await reaction.message.guild?.members.fetch(user.id);
     if (!member) return true; // Handled, but failed
-
+    0;
     // Only DEs can use gold
     if (!member.roles.cache.has(roles.deatheaters)) return true;
 
@@ -79,16 +67,15 @@ export const ReactionHandler: CommandReactionHandler = async ({ reaction, user, 
             customID: "yes",
             style: "SUCCESS",
             label: "Yes (5000 credits)",
-            emoji: { name: "gold", id: "389216023141941249" } as any
+            emoji: { name: "gold", id: "389216023141941249" } as EmojiIdentifierResolvable
         }),
         new MessageButton({ customID: "cancel", style: "DANGER", label: "Cancel" })
     ]);
 
-    const m = await msg.reply({
-        content: member.toString(),
+    const dm = await member.createDM();
+    const m = await dm.send({
         embeds: [embed],
-        components: [actionRow],
-        allowedMentions: { repliedUser: false, users: [member.user.id] }
+        components: [actionRow]
     });
 
     const dtx = MessageContext(m);
@@ -96,18 +83,22 @@ export const ReactionHandler: CommandReactionHandler = async ({ reaction, user, 
     dtx.registerComponent("yes", async (interaction) => {
         if (interaction.user.id !== user.id) return;
 
-        const goldMessage = await giveGold(msg, connection);
-        if (!goldMessage) {
+        try {
+            const goldMessage = await giveGold(msg, member, connection);
+
+            const actionRow = new MessageActionRow().addComponents([
+                new MessageButton({ style: "LINK", label: "View post", url: goldMessage.url })
+            ]);
+
+            embed.setTitle("Message was given gold!");
+            m.edit({ embeds: [embed], components: [actionRow] });
+        } catch (e) {
             await m.delete();
-            throw new CommandError("An error occurred");
+            const err = e instanceof CommandError ? e : new CommandError("An error occurred");
+
+            await dm.send({ embeds: [new MessageEmbed().setDescription(err.message)] });
         }
-
-        const actionRow = new MessageActionRow().addComponents([
-            new MessageButton({ style: "LINK", label: "View post", url: goldMessage.url })
-        ]);
-
-        embed.setTitle("Message was given gold!");
-        m.edit({ embeds: [embed], components: [actionRow] });
+        dtx.unregisterComponent("yes");
     });
 
     dtx.registerComponent("cancel", async (interaction) => {
@@ -115,12 +106,19 @@ export const ReactionHandler: CommandReactionHandler = async ({ reaction, user, 
 
         embed.setTitle("This message was not given gold.");
         m.edit({ embeds: [embed], components: [] });
+        dtx.unregisterComponent("cancel");
     });
 
     return true;
 };
 
-async function giveGold(msg: Message, connection: Connection): Promise<Message | null> {
+async function giveGold(msg: Message, givingMember: GuildMember, connection: Connection): Promise<Message> {
+    // You can't gold yourself
+    if (msg.author.id === givingMember.id) throw new CommandError("You cannot give yourself gold.");
+
+    // Need to combine to give a truly unique id since multiple people can gold the same message
+    const pollID = `${msg.id}+${givingMember.id}`;
+
     // Give user gold
     const golds =
         (await connection.getRepository(Counter).findOne({ identifier: msg.author.id, title: "GoldCount" })) ||
@@ -131,25 +129,28 @@ async function giveGold(msg: Message, connection: Connection): Promise<Message |
     await connection.manager.save(golds);
 
     // Take away credits
-    const userEconomy = await connection.getRepository(Economy).findOne({ userid: msg.author.id });
+    const userEconomy = await connection.getRepository(Economy).findOne({ userid: givingMember.id });
     if (!userEconomy || userEconomy.credits < 5000) throw new CommandError("You don't have enough credits to give gold!"); // prettier-ignore
-
     userEconomy.credits -= 5000;
-    await connection.manager.save(userEconomy);
+
+    // Ensure a poll doesn't already exist
+    const oldPoll = await connection.getRepository(Poll).findOne({ identifier: pollID });
+    if (oldPoll) throw new CommandError("You've already given this message gold!");
 
     // Create poll object to save votes
-    const poll = new Poll({ identifier: msg.id, userid: msg.author.id });
+    const poll = new Poll({ identifier: pollID, userid: givingMember.id });
     await connection.manager.save(poll);
 
     // Create embed
-    if (!msg.member) return null;
+    if (!msg.member) throw new Error("Unable to locate member");
 
     const embed = new MessageEmbed()
         .setAuthor(msg.member.displayName, msg.author.displayAvatarURL())
         .setColor("#FCE300")
         .setDescription(msg.content)
-        .addField("Channel", (msg.channel as TextChannel).name, true)
-        .addField("Score", `0`, true)
+        .addField("Channel", `${msg.channel}`, true)
+        .addField("Posted", F.discordTimestamp(msg.createdAt), true)
+        .addField("Given gold by", `${givingMember}`)
         .setFooter(
             `x${golds.count} | Vote on this post by clicking one of the buttons below. Your vote will remain anonymous.`,
             "http://i.imgur.com/QTzrs2Y.png"
@@ -163,20 +164,26 @@ async function giveGold(msg: Message, connection: Connection): Promise<Message |
     const actionRow = new MessageActionRow();
     actionRow.addComponents([
         new MessageButton({
-            customID: answerListener.generateCustomID({ index: "1", pollID: msg.id }),
+            customID: answerListener.generateCustomID({ index: "1", pollID }),
             style: "SECONDARY",
             label: "0",
-            emoji: { name: "upvote_pink2", id: "850586748765077514" } as any
+            emoji: { name: "upvote_pink2", id: "850586748765077514" } as EmojiIdentifierResolvable
         }),
         new MessageButton({
-            customID: answerListener.generateCustomID({ index: "0", pollID: msg.id }),
+            customID: answerListener.generateCustomID({ index: "0", pollID }),
             style: "SECONDARY",
             label: "0",
-            emoji: { name: "downvote_blue2", id: "850586787805265990" } as any
+            emoji: { name: "downvote_blue2", id: "850586787805265990" } as EmojiIdentifierResolvable
+        }),
+        new MessageButton({
+            url: msg.url,
+            style: "LINK",
+            label: "View message"
         })
     ]);
 
     const m = await chan.send({ embeds: [embed], components: [actionRow] });
 
+    await connection.manager.save(userEconomy);
     return m;
 }

16981a684cbb9b8bcc3876c27374c7fe6aa4c29a
Cameron Haley <chaley@hawk.iit.edu>
2021-07-19 17:08:18 -0500

    Add defualt values to appropriate schema fields

--- src/slashcommands/staff/test.ts
+++ src/slashcommands/staff/test.ts
@@ -2,16 +2,30 @@ import { PrismaClient } from "@prisma/client";
 import { CommandOptions, CommandRunner } from "configuration/definitions";
 import { Economy } from "../../database/entities/Economy";
 
-const prisma = new PrismaClient();
-
 export const Options: CommandOptions = {
     description: "Test command",
     options: []
 };
 
 export const Executor: CommandRunner = async (ctx) => {
-    const user = await prisma.user.findUnique({ where: { id: 75345735n }, include: { dailyBox: true, tags: true } });
-    if (!user) return;
-
-    user.tags;
+    // const user = await ctx.prisma.user.findUnique({
+    //     where: { id: BigInt(ctx.member.id) },
+    //     include: { dailyBox: true, tags: true }
+    // });
+    // if (!user) return;
+    // user.joinedAt = new Date();
+    // await ctx.prisma.user.create({
+    //     data: {
+    //         id: BigInt(ctx.member.id),
+    //         dailyBox: { create: {} }
+    //     }
+    // });
+    // ctx.prisma.$transaction([ctx.prisma.user.update({ where: { id: user.id }, data: { score: { increment: 200 } } })]);
+    // const tagName = "whatsup";
+    // ctx.prisma.$queryRaw`
+    //     SELECT * FROM tags WHERE name=${tagName}
+    //     JOIN users
+    // `;
+    // await prisma.user.update({ where: { id: user.id }, data: { score: { increment: 200 } } });
+    // user.tags;
 };

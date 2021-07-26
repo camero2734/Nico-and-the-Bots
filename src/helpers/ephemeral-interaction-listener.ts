d96a34b5e1ba03b0440384a792ee4d117c5720e1
Cameron Haley <chaley@hawk.iit.edu>
2021-07-25 20:24:48 -0500

    Add inline interaction listener for expectedly short actions (like yes/no buttons) to avoid complexity

---
+++ src/helpers/ephemeral-interaction-listener.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { CollectorFilter, TextChannel, MessageComponentInteraction } from "discord.js";
+import { Message } from "discord.js";
+import F from "./funcs";
+import { ExtendedInteraction } from "./slash-command";
+
+type IDsMapped<IDs extends Readonly<string[]>> = {
+    [Index in keyof IDs]: IDs[Index] extends string ? `eph&${IDs[Index]}${string}` : never;
+};
+
+export class EphemeralInteractionListener<IDs extends Readonly<string[]>> {
+    customIDs: IDsMapped<IDs>;
+    constructor(private ctx: ExtendedInteraction, private names: IDs) {
+        this.customIDs = names.map((n) => {
+            const hash = F.hash(names.join(",")).slice(0, 4);
+            return `eph&${n}${hash}`;
+        }) as any;
+    }
+    async wait(): Promise<IDsMapped<IDs>[number] | null> {
+        const promises = this.customIDs.map((customID) => {
+            return new Promise<string | null>((resolve) => {
+                const timeout = setTimeout(() => resolve(null), 120 * 1000);
+
+                const filter: CollectorFilter<[MessageComponentInteraction]> = (interaction) => interaction.customId === customID; // prettier-ignore
+                const collector = this.ctx.channel.createMessageComponentCollector({ filter });
+
+                collector.on("collect", () => {
+                    clearTimeout(timeout);
+                    resolve(customID);
+                });
+                collector.on("end", () => console.log(`${customID} collector ended`));
+            });
+        });
+
+        const result = await Promise.race(promises); // We only care about the first button pressed
+        return result as IDsMapped<IDs>[number] | null;
+    }
+}

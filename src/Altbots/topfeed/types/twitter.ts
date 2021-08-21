45e3bcfca021600008fa9f4430d9032f62f33c67
pootusmaximus <ckhaley21@gmail.com>
2021-08-20 20:40:18 -0500

    Use twitter-api-v2 package instead of making requests from scratch

--- src/Altbots/topfeed/types/twitter.ts
+++ src/Altbots/topfeed/types/twitter.ts
@@ -1,87 +1,8 @@
 import { MessageEmbed, MessageOptions } from "discord.js";
-import F from "../../../Helpers/funcs";
-import fetch from "node-fetch";
+import TwitterApi from "twitter-api-v2";
 import secrets from "../../../Configuration/secrets";
+import F from "../../../Helpers/funcs";
 import { Checked, Watcher } from "./base";
-
-export interface ReferencedTweet {
-    type: string;
-    id: string;
-}
-
-export interface PublicMetrics {
-    retweet_count: number;
-    reply_count: number;
-    like_count: number;
-    quote_count: number;
-}
-
-export interface Attachments {
-    media_keys: string[];
-}
-
-export interface TweetData {
-    id: string;
-    referenced_tweets?: ReferencedTweet[];
-    author_id: string;
-    source: string;
-    public_metrics: PublicMetrics;
-    created_at: Date;
-    text: string;
-    in_reply_to_user_id: string;
-    attachments?: Attachments;
-}
-
-export interface PublicMetrics {
-    retweet_count: number;
-    reply_count: number;
-    like_count: number;
-    quote_count: number;
-}
-
-export interface TweetUser {
-    username: string;
-    profile_image_url: string;
-    id: string;
-    name: string;
-}
-
-export interface Tweet {
-    attachments: Attachments[];
-    source: string;
-    author_id: string;
-    id: string;
-    text: string;
-    created_at: Date;
-    public_metrics: PublicMetrics[];
-}
-
-export interface Medium {
-    media_key: string;
-    type: string;
-    url?: string;
-    preview_image_url?: string;
-}
-
-export interface Includes {
-    users: TweetUser[];
-    tweets: Tweet[];
-    media: Medium[];
-}
-
-type IDResponse = { id: string; name: string; username: string };
-
-async function makeRequest<T, U = undefined>(endpoint: string): Promise<{ data: T; includes: U }> {
-    const url = `https://api.twitter.com/2/${endpoint}`;
-    const headers = {
-        Authorization: `Bearer ${secrets.apis.twitter.bearer_token}`
-    };
-    const res = await fetch(url, { headers });
-    return await res.json();
-}
-
-const _getID = (handle: string) => makeRequest<IDResponse>(`users/by/username/${handle}`);
-const _getTweets = (id: string) => makeRequest<TweetData[], Includes>(`users/${id}/tweets?expansions=referenced_tweets.id,referenced_tweets.id.author_id,attachments.media_keys&tweet.fields=created_at,geo,public_metrics,in_reply_to_user_id,referenced_tweets,source&media.fields=preview_image_url,type,url&user.fields=profile_image_url,username&max_results=5`); // prettier-ignore
 
 type TweetType = {
     images: string[];
@@ -93,28 +14,43 @@ type TweetType = {
     tweetText: string;
 };
 
+const twitter = new TwitterApi(secrets.apis.twitter.bearer_token).readOnly;
+
 export class TwitterWatcher extends Watcher<TweetType> {
     type = "Twitter" as const;
     userid: string;
     async fetchRecentItems(): Promise<Checked<TweetType>[]> {
         if (!this.userid) await this.fetchUserID();
 
-        const { data, includes } = await _getTweets(this.userid);
+        const { tweets, includes } = await twitter.v2.userTimeline(this.userid, {
+            expansions: ["referenced_tweets.id", "referenced_tweets.id.author_id", "attachments.media_keys"],
+            "tweet.fields": [
+                "created_at",
+                "geo",
+                "public_metrics",
+                "in_reply_to_user_id",
+                "referenced_tweets",
+                "source"
+            ],
+            "media.fields": ["preview_image_url", "type", "url"],
+            "user.fields": ["profile_image_url", "username"],
+            max_results: 5
+        });
 
-        return data.map((tweet) => {
+        return tweets.map((tweet) => {
             const images = (tweet.attachments?.media_keys || [])
-                .map((mk) => includes.media.find((m) => m.media_key === mk))
+                .map((mk) => includes.media?.find((m) => m.media_key === mk))
                 .map((m) => m?.url || m?.preview_image_url || "")
                 .filter((m) => m);
 
-            const date = new Date(tweet.created_at);
+            const date = new Date(tweet.created_at || Date.now());
 
-            const referencedTweet = tweet.referenced_tweets?.[0] || { type: undefined, id: undefined };
-            const tweetType = referencedTweet.type ? F.titleCase(referencedTweet.type) : "Tweeted";
+            const referencedTweet = tweet.referenced_tweets?.[0];
+            const tweetType = referencedTweet?.type ? F.titleCase(referencedTweet.type) : "Tweeted";
 
-            const nameRes = includes.users.find((u) => u.id === referencedTweet.id);
+            const nameRes = includes.users?.find((u) => u.id === referencedTweet?.id);
             const tweeterUsername = tweetType === "Retweeted" && nameRes ? nameRes.username : this.handle;
-            const tweeterImage = includes.users.find((u) => u.id === (nameRes?.id || this.userid))?.profile_image_url;
+            const tweeterImage = includes.users?.find((u) => u.id === (nameRes?.id || this.userid))?.profile_image_url;
 
             return {
                 uniqueIdentifier: tweet.id,
@@ -162,7 +98,7 @@ export class TwitterWatcher extends Watcher<TweetType> {
     }
 
     async fetchUserID(): Promise<void> {
-        const res = await _getID(this.handle);
+        const res = await twitter.v2.userByUsername(this.handle);
         this.userid = res.data.id;
     }
 }

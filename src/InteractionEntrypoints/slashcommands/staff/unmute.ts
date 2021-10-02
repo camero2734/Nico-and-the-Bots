321a3fa4923a7ad7e5a66e2a9cc38fc132f3c7ac
pootusmaximus <ckhaley21@gmail.com>
2021-10-02 14:32:54 -0500

    Add unmute command

---
+++ src/InteractionEntrypoints/slashcommands/staff/unmute.ts
@@ -0,0 +1,29 @@
+import { MessageEmbed } from "discord.js";
+import { roles } from "../../../Configuration/config";
+import { prisma } from "../../../Helpers/prisma-init";
+import { SlashCommand } from "../../../Structures/EntrypointSlashCommand";
+
+const command = new SlashCommand(<const>{
+    description: "Mutes a user",
+    options: [{ name: "user", description: "The user to unmute", required: true, type: "USER" }]
+});
+
+command.setHandler(async (ctx) => {
+    const { user } = ctx.opts;
+    await ctx.deferReply();
+
+    const member = await ctx.member.guild.members.fetch(user);
+    await member.roles.remove(roles.muted);
+    await member.roles.add(roles.banditos);
+
+    // Mark any current mutes as finished
+    await prisma.mute.updateMany({
+        where: { mutedUserId: member.id },
+        data: { finished: true }
+    });
+
+    const embed = new MessageEmbed().setDescription(`${member} has been unmuted!`);
+    await ctx.send({ embeds: [embed] });
+});
+
+export default command;

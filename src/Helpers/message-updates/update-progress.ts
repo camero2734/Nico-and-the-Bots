86cd1f91513fccd981b4eff6fa08da228790dca4
pootusmaximus <ckhaley21@gmail.com>
2021-10-02 13:24:24 -0500

    Add server update message update

--- src/Helpers/message-updates/update-progress.ts
+++ src/Helpers/message-updates/update-progress.ts
@@ -1,18 +1,45 @@
+import { differenceInMilliseconds, parse } from "date-fns";
 import { Message, MessageEmbed, MessageOptions } from "discord.js";
-import { FooterEncoder } from "./_footer-encoder";
+import progressBar from "string-progressbar";
+import { channelIDs, emojiIDs } from "../../Configuration/config";
+import F from "../funcs";
 import { MessageUpdate } from "./_queue";
 
-interface FooterData {
-    count: number;
-}
+const startDate = parse("02 October 2021 12:00", "dd MMMM yyyy HH:mm", new Date());
+const endDate = parse("02 October 2021 17:00", "dd MMMM yyyy HH:mm", new Date());
 
-const footerEncoder = new FooterEncoder<FooterData>({ count: 0 });
+const toEmoji = (id: string) => `<:name:${id}>`;
+const startEmoji = toEmoji(emojiIDs.poll.start);
+const filledEmoji = toEmoji(emojiIDs.poll.filled);
+const emptyEmoji = toEmoji(emojiIDs.poll.empty);
+const endEmoji = toEmoji(emojiIDs.poll.end);
+
+const generateProgressBar = (): [string, boolean] => {
+    const now = new Date();
+    const elapsedTime = differenceInMilliseconds(now, startDate);
+    const totalTime = differenceInMilliseconds(endDate, startDate);
+
+    console.log(elapsedTime, totalTime);
+
+    const [progress] = progressBar.filledBar(totalTime, elapsedTime, 8, emptyEmoji, filledEmoji);
+    return [`${startEmoji}${progress}${endEmoji}\u200b`, elapsedTime > totalTime];
+};
+
+const standardizeEmbed = (embed: MessageEmbed): void => {
+    embed.fields = [];
+    embed
+        .setAuthor("DEMAtronix™ Telephony System", "https://i.imgur.com/csHALvp.png")
+        .setColor("#7289DA")
+        .addField("Performing system upgrade...", `Expected to finish ${F.discordTimestamp(endDate, "relative")}`)
+        .setFooter("Last updated")
+        .setTimestamp(new Date());
+};
 
 const initialMessage = async (): Promise<MessageOptions> => {
-    const embed = new MessageEmbed() //
-        .setTitle("This is a test")
-        .setDescription("0/100")
-        .setFooter(footerEncoder.encode({ count: 0 }));
+    const [progress] = generateProgressBar();
+
+    const embed = new MessageEmbed().setDescription(progress);
+    standardizeEmbed(embed);
 
     return {
         embeds: [embed]
@@ -20,21 +47,23 @@ const initialMessage = async (): Promise<MessageOptions> => {
 };
 
 const update = async (msg: Message) => {
-    const embed = msg.embeds[0];
-    const footerText = embed.footer?.text as string;
-    const footerData = footerEncoder.decode(footerText);
-    footerData.count += 1;
+    const [progress, isDone] = generateProgressBar();
 
-    embed.setDescription(`${footerData.count}/100`);
-    embed.setFooter(footerEncoder.encode(footerData));
+    if (!isDone) {
+        const embed = msg.embeds[0];
+        embed.setDescription(progress);
+        standardizeEmbed(embed);
 
-    await msg.edit({ embeds: [embed] });
+        await msg.edit({ embeds: [embed] });
+    } else {
+        // TODO
+    }
 };
 
 export const UpdateProgress = {
     name: "update-progress",
     initialMessage,
     update,
-    channelId: "859592952108285992",
+    channelId: channelIDs.announcements,
     intervalMinutes: 0.5
 } as MessageUpdate;

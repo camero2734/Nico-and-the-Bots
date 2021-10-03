75fdfb4a638ec7aa14d8c6e19749cc3a3660dca6
pootusmaximus <ckhaley21@gmail.com>
2021-10-03 10:49:01 -0500

    Fix top command, move roleinfo command to /info category

--- src/InteractionEntrypoints/slashcommands/econ/top.ts
+++ src/InteractionEntrypoints/slashcommands/econ/top.ts
@@ -60,7 +60,7 @@ command.setHandler(async (ctx) => {
 
     await ctx.send({
         // content: `Took ${Date.now() - startTime} ms (${timeStamp} ms) to fetch ${memberScores.length} items`,
-        content: "",
+        embeds: [],
         files: [{ name: `top-over${timeperiod || 0}-page${pageNum}.png`, attachment: buffer }]
     });
 });

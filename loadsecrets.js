707dd5709813cafac1a4f336fa893d8b4f03f452
pootusmaximus <ckhaley21@gmail.com>
2021-10-04 18:51:33 -0500

    Fix case sensitivity issues

--- loadsecrets.js
+++ loadsecrets.js
@@ -2,15 +2,17 @@
 const { spawnSync } = require("child_process");
 
 const files = {
-    "secrets.ts": "src/configuration/secrets.ts",
+    "secrets.ts": "src/Configuration/secrets.ts",
     "quiz.ts": "src/helpers/verified-quiz/quiz.ts"
 };
 
 if (process.env.RENDER) {
+    console.log("[Load Secrets] Copying files...");
     for (const [from, to] of Object.entries(files)) {
         const fromPath = `/etc/secrets/${from}`;
         const toPath = `dist/${to}`;
 
         spawnSync("cp", [fromPath, toPath]);
     }
+    console.log("[Load Secrets] Done");
 }

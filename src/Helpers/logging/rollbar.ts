83d3bf557ba6a9a33ee06746af759891b671bf60
pootusmaximus <ckhaley21@gmail.com>
2021-10-05 23:51:22 -0500

    Update env detection

--- src/Helpers/logging/rollbar.ts
+++ src/Helpers/logging/rollbar.ts
@@ -1,11 +1,9 @@
 import Rollbar from "rollbar";
 
-const onHeroku = process.env.ON_HEROKU === "1";
-
 export const rollbar = new Rollbar({
     accessToken: "ec97643e6fd844998535e9cc6832c024",
     captureUncaught: true,
     captureUnhandledRejections: true,
-    environment: onHeroku ? "production" : "development",
+    environment: process.env.NODE_ENV || "development",
     verbose: true
 });

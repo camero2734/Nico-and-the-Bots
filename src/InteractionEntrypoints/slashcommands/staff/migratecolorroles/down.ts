69eb8c3d13d99037bbfc12f3a65497c7524f07b9
pootusmaximus <ckhaley21@gmail.com>
2021-10-08 23:45:30 -0500

    Reenable song role command

--- src/InteractionEntrypoints/slashcommands/staff/migratecolorroles/down.ts
+++ src/InteractionEntrypoints/slashcommands/staff/migratecolorroles/down.ts
@@ -8,12 +8,14 @@ const command = new SlashCommand(<const>{
 });
 
 command.setHandler(async (ctx) => {
+    await ctx.deferReply();
     for (const [roleId, change] of Object.entries(changes) as [Snowflake, Change][]) {
         const role = await ctx.guild.roles.fetch(roleId);
         if (!role) continue;
 
         await role.edit({ color: change.from });
     }
+    await ctx.editReply("Done");
 });
 
 export default command;

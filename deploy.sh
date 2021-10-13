510fa202ef2675a2122c330fd69234a415511e2a
pootusmaximus <ckhaley21@gmail.com>
2021-10-12 21:42:59 -0500

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,12 @@
-files=("./src/configuration/secrets.ts" "./src/helpers/verified-quiz/quiz.ts")
-fileVals=("${a[@]}") 
+vultr_deploy # Local alias that triggers new deployment
 
-git switch heroku && # Switch to branch that has secret files
-git add -A && # Add any new files
-git merge main -s recursive -X ours && # Ignore different .gitignore file
-git push -u heroku +HEAD:master # Push to heroku main branch
 
-# Save hidden files b/c git removes them when switching back
-for i in ${!files[@]};
-do
-    fileVals[$i]=$(cat "${files[$i]}")
-done
+echo "Waiting for deploy..."
 
-git switch main
-git add -A
+# This command will be killed when the deploy is finished
+vultr "sleep 678" # ssh ... "sleep 678"
 
-# Rewrite hidden files
-for i in ${!files[@]};
-do
-    echo "${fileVals[$i]}" > "${files[$i]}"
-done
+echo "Deploy done. Killing local processes"
+
+# Kill local running process
+killall node
--- src/InteractionEntrypoints/slashcommands/staff/test.ts
+++ src/InteractionEntrypoints/slashcommands/staff/test.ts
@@ -12,15 +12,6 @@ const command = new SlashCommand(<const>{
 command.setHandler(async (ctx) => {
     await ctx.deferReply();
 
-    const msg = await F.fetchMessageFromUrl(
-        "https://discord.com/channels/269657133673349120/827679833487704094/897619370845569024",
-        ctx.guild
-    );
-
-    if (!msg) throw new CommandError("No msg");
-
-    updateUserScore(msg);
-
     await ctx.editReply({ content: "ok" });
 });
 

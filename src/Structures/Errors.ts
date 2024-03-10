69af83f814d3580ceb31c57e278b007ce0e34664
camero2734 <ckhaley21@gmail.com>
2024-03-10 15:15:36 +0100

    Add error ids so I can look up errors later

--- src/Structures/Errors.ts
+++ src/Structures/Errors.ts
@@ -1,5 +1,6 @@
 import { CommandInteraction, DMChannel, EmbedBuilder, Interaction, TextChannel } from "discord.js";
 import { CommandError } from "../Configuration/definitions";
+import { nanoid } from "nanoid";
 
 const getReplyMethod = async (ctx: CommandInteraction) => {
     if (!ctx.isRepliable()) return ctx.followUp;
@@ -9,9 +10,12 @@ const getReplyMethod = async (ctx: CommandInteraction) => {
 }
 
 export const ErrorHandler = async (ctx: TextChannel | DMChannel | Interaction, e: unknown) => {
+    const errorId = nanoid();
+
     console.log("===================================");
     console.log("||                               ||");
     console.log(`----> ${(e as object).constructor.name} Error!`);
+    console.log(`----> Error ID: ${errorId}`);
     console.log("||                               ||");
     console.log("===================================");
     if (e instanceof Error) console.log(e.stack);
@@ -26,7 +30,7 @@ export const ErrorHandler = async (ctx: TextChannel | DMChannel | Interaction, e
             .setDescription(e.message)
             .setTitle("An error occurred!")
             .setColor("DarkRed")
-            .setFooter({ text: "DEMA internet machine broke" });
+            .setFooter({ text: `DEMA internet machine broke. Error ${errorId}` });
         ectx.send({
             embeds: [embed],
             components: [],
@@ -37,7 +41,7 @@ export const ErrorHandler = async (ctx: TextChannel | DMChannel | Interaction, e
         console.log(`Unknown error:`, e);
         const embed = new EmbedBuilder()
             .setTitle("An unknown error occurred!")
-            .setFooter({ text: "DEMA internet machine really broke" });
+            .setFooter({ text: `DEMA internet machine really broke. Error ${errorId}` });
         ectx.send({ embeds: [embed], components: [], ephemeral: true });
     }
 };

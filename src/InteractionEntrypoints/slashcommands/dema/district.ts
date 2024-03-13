462c8ad0fde0663f49fce44af7868147198fca79
camero2734 <ckhaley21@gmail.com>
2024-03-13 22:54:44 +0100

    Add some waiting messages before revealing district

--- src/InteractionEntrypoints/slashcommands/dema/district.ts
+++ src/InteractionEntrypoints/slashcommands/dema/district.ts
@@ -3,6 +3,14 @@ import { roles, userIDs } from "../../../Configuration/config";
 import { CommandError } from "../../../Configuration/definitions";
 import F from "../../../Helpers/funcs";
 import { SlashCommand } from "../../../Structures/EntrypointSlashCommand";
+import { Faker, en } from "@faker-js/faker";
+
+const loadingMessages = [
+    "Verifying residency eligibility based on Dema's citizenship criteria",
+    "Conducting assessments for integration into Dema's societal fabric",
+    "Enhancing indoctrination procedures to ensure undying allegiance to Vialism",
+    "Fetching failed perimeter escape records to cross-check with your application",
+];
 
 const command = new SlashCommand({
     description: "Receive your district assignment in DEMA",
@@ -20,18 +28,48 @@ command.setHandler(async (ctx) => {
     if (hasRole) throw new CommandError(`You have already been assigned to ${hasRole.name.toUpperCase()}.`);
 
     // Assign a district role
-    const assignedRoleId = F.randomValueInArray(districtRoleIds);
+    const faker = new Faker({ locale: [en] });
+    faker.seed(F.hashToInt(`district:${ctx.user.id}`));
+    const assignedRoleId = faker.helpers.arrayElement(districtRoleIds);
     const role = await ctx.guild.roles.fetch(assignedRoleId);
     if (!role) throw new CommandError("Invalid role");
 
-    await ctx.member.roles.add(role);
-
+    // Now have some fun -- pretend to think about it for a few seconds
     const embed = new EmbedBuilder()
-        .setColor(Colors.Gold)
-        .setTitle("District assignment")
-        .setDescription(`You have been assigned to ${role.name}`);
+        .setTitle("Digital Entry Management Assistant Relocation (DEMA-R) program")
+        .setDescription("Thank you for choosing the Dema relocation program. We are now processing your application.")
+        .setColor(Colors.Red);
 
     await ctx.editReply({ embeds: [embed] });
+
+    const displayedMessages = faker.helpers.shuffle(loadingMessages).slice(0, 3);
+
+    for (const msg of displayedMessages) {
+        await F.wait(300);
+        embed.addFields({
+            name: msg,
+            value: "..."
+        });
+
+        await ctx.editReply({ embeds: [embed] });
+    }
+
+    await F.wait(1000);
+
+    const bishopName = Object.entries(roles.districts).find(x => x[1] === assignedRoleId)?.[0]?.toUpperCase();
+
+    const finalEmbed = new EmbedBuilder()
+        .setTitle("Digital Entry Management Assistant Relocation (DEMA-R) program")
+        .setDescription("Your application has been processed.")
+        .setColor(Colors.Red)
+        .addFields({
+            name: "District Assignment",
+            value: `You have been assigned to ${role.name.toUpperCase()}. ${bishopName} welcomes you to your new home.`
+        });
+
+    await ctx.editReply({ embeds: [finalEmbed] });
 });
+
+
 
 export default command;

bbb8b9005a034346713f19ac4bcb3f42f1806c53
camero2734 <ckhaley21@gmail.com>
2024-03-15 23:11:58 +0100

    Score

--- src/Helpers/get-badges.ts
+++ src/Helpers/get-badges.ts
@@ -237,20 +237,10 @@ async function districtBadge(bishop: string, rgb: [number, number, number]): Pro
 
     ctx.drawImage(image, 0, 0, 500, 500);
 
-    ctx.save();
-    ctx.font = "bold 18px Futura";
-    ctx.fillStyle = "white";
-    ctx.textAlign = "center";
-    ctx.letterSpacing = "10px";
-    ctx.globalAlpha = 1;
-    ctx.fillText("I AM A", 251, 200);
-    ctx.fillText("CITIZEN", 251, 230);
-    ctx.restore();
-
     const imgData = ctx.getImageData(0, 0, 500, 500);
     const data = imgData.data;
 
-    const colorFrom = [0, 0, 0];
+    const colorFrom = [43, 43, 40];
     const colorTo = rgb;
 
     for (let i = 0; i < data.length; i += 4) {
@@ -259,7 +249,7 @@ async function districtBadge(bishop: string, rgb: [number, number, number]): Pro
         const b = data[i + 2];
 
         const row = Math.floor(i / 2000);
-        const brightness = (row / 500) * ((r + g + b) / 3) / 255;
+        const brightness = Math.min(0.3 + (row / 500) * ((r + g + b) / 3) / 255, 1);
 
         data[i] = F.lerp(brightness, colorFrom[0], colorTo[0]);
         data[i + 1] = F.lerp(brightness, colorFrom[1], colorTo[1]);
@@ -274,9 +264,18 @@ async function districtBadge(bishop: string, rgb: [number, number, number]): Pro
     ctx.letterSpacing = "30px";
     ctx.fillText(bishop.toUpperCase(), 265, 470);
 
+    ctx.save();
+    ctx.font = "bold 18px Futura";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.letterSpacing = "10px";
+    ctx.fillText("I AM A", 251, 200);
+    ctx.fillText("CITIZEN", 251, 230);
+    ctx.restore();
+
     // Border around the image
     ctx.strokeStyle = ctx.fillStyle;
-    ctx.lineWidth = 10;
+    ctx.lineWidth = 25;
     ctx.strokeRect(0, 0, 500, 500);
 
     return await loadImage(canvas.toBuffer("image/png"));

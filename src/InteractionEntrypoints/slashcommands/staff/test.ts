222df9868d58a2ed437e2c6b059bcfeaaec2e6a7
camero2734 <ckhaley21@gmail.com>
2024-03-28 21:44:08 +0100

    Delete all the existing concert roles

--- src/InteractionEntrypoints/slashcommands/staff/test.ts
+++ src/InteractionEntrypoints/slashcommands/staff/test.ts
@@ -1,7 +1,6 @@
-import { ApplicationCommandOptionType } from "discord.js";
+import { ApplicationCommandOptionType, roleMention } from "discord.js";
 import { userIDs } from "../../../Configuration/config";
 import { SlashCommand } from "../../../Structures/EntrypointSlashCommand";
-import { getConcertChannelManager } from "../../scheduled/concert-channels";
 
 const command = new SlashCommand({
     description: "Test command",
@@ -21,11 +20,23 @@ command.setHandler(async (ctx) => {
     await ctx.deferReply({ ephemeral: true });
 
     // Concert channels
-    const concertChannelManager = getConcertChannelManager(ctx.guild);
-    await concertChannelManager.initialize(ctx.opts.num);
-    await concertChannelManager.checkChannels();
+    // const concertChannelManager = getConcertChannelManager(ctx.guild);
+    // await concertChannelManager.initialize(ctx.opts.num);
+    // await concertChannelManager.checkChannels();
 
-    await ctx.editReply("Done");
+    const roles = await ctx.guild.roles.fetch();
+    const withColor = roles.filter(r => r.hexColor.toLowerCase() === "#ffc6d5");
+    if (ctx.opts.num === 42) {
+        for (const role of withColor.values()) {
+            await role.delete();
+        }
+
+        await ctx.editReply("Done");
+    } else {
+        const msg = withColor.map(x => roleMention(x.id)).join("\n");
+
+        await ctx.editReply(msg);
+    }
 });
 
 export default command;

98171d122a827c4ec441f9115f67e0c4063fc5fe
camero2734 <ckhaley21@gmail.com>
2024-04-03 16:55:04 +0200

    Experiment adding public ban logs

--- src/InteractionEntrypoints/slashcommands/staff/ban.ts
+++ src/InteractionEntrypoints/slashcommands/staff/ban.ts
@@ -1,5 +1,5 @@
-import { ApplicationCommandOptionType, Colors, EmbedBuilder, TextChannel } from "discord.js";
-import { channelIDs, roles } from "../../../Configuration/config";
+import { ActionRowBuilder, ApplicationCommandOptionType, ButtonBuilder, ButtonStyle, Colors, EmbedBuilder, ModalBuilder, TextChannel, TextInputBuilder, TextInputStyle } from "discord.js";
+import { channelIDs, roles, userIDs } from "../../../Configuration/config";
 import { CommandError } from "../../../Configuration/definitions";
 import { MessageTools } from "../../../Helpers";
 import { SlashCommand } from "../../../Structures/EntrypointSlashCommand";
@@ -52,15 +52,66 @@ command.setHandler(async (ctx) => {
     }
 
     await MessageTools.safeDM(member, { embeds: [bannedEmbed] });
-    await member.ban({ deleteMessageDays: purge ? 7 : 0, reason });
+
+    if (ctx.user.id !== userIDs.myAlt) {
+        await member.ban({ deleteMessageDays: purge ? 7 : 0, reason });
+    }
+
     await ctx.send({ embeds: [new EmbedBuilder({ description: `${member.toString()} was banned.` }).toJSON()] });
 
-    sendToBanLog(ctx, bannedEmbed);
+    // Send to ban log
+    const actionRow = new ActionRowBuilder<ButtonBuilder>().setComponents(
+        new ButtonBuilder()
+            .setLabel("Update Action Details")
+            .setEmoji("📝")
+            .setStyle(ButtonStyle.Secondary)
+            .setCustomId(genBtnId({}))
+    );
+
+    bannedEmbed.setFooter({ text: "If you have any questions about this action, feel free to open a #support ticket." });
+
+    const banLogChannel = (await ctx.guild.channels.fetch(channelIDs.banlog)) as TextChannel;
+    await banLogChannel.send({ embeds: [bannedEmbed], components: [actionRow] });
 });
 
-async function sendToBanLog(ctx: typeof command.ContextType, bannedEmbed: EmbedBuilder) {
-    const banLogChannel = (await ctx.guild.channels.fetch(channelIDs.banlog)) as TextChannel;
-    await banLogChannel.send({ embeds: [bannedEmbed] });
-}
+const FIELD_PUBLIC_BAN_REASON = "banReasonPublic";
+const FIELD_INTERNAL_BAN_REASON = "banReasonInternal";
+const genBtnId = command.addInteractionListener("editBanDetails", [], async (ctx) => {
+    if (!ctx.isButton()) return;
+
+    const modal = new ModalBuilder()
+        .setCustomId(genSubmitId({}))
+        .setTitle("Edit ban details");
+
+    const actionRow = new ActionRowBuilder<TextInputBuilder>().setComponents([
+        new TextInputBuilder()
+            .setLabel("Public ban reason")
+            .setPlaceholder("Reason for banning. Be as thorough as possible.")
+            .setCustomId(FIELD_PUBLIC_BAN_REASON)
+            .setRequired(true)
+            .setStyle(TextInputStyle.Paragraph),
+        new TextInputBuilder()
+            .setLabel("Internal ban reason")
+            .setPlaceholder("Reason for banning if some details aren't appropriate to be shared. Only available to staff.")
+            .setCustomId(FIELD_INTERNAL_BAN_REASON)
+            .setRequired(true)
+            .setStyle(TextInputStyle.Paragraph)
+    ]);
+
+    modal.setComponents(actionRow);
+
+    await ctx.showModal(modal);
+});
+
+const genSubmitId = command.addInteractionListener("editBanDetailsSubmit", [], async (ctx) => {
+    if (!ctx.isModalSubmit()) return;
+
+    await ctx.deferReply({ ephemeral: true });
+
+    const newBanReason = ctx.fields.getTextInputValue(FIELD_PUBLIC_BAN_REASON);
+    const newInternalBanReason = ctx.fields.getTextInputValue(FIELD_INTERNAL_BAN_REASON);
+
+    await ctx.editReply({ content: `Would update to:\n\n${newBanReason}. Also would note this down privately:\n\n${newInternalBanReason}` });
+});
 
 export default command;

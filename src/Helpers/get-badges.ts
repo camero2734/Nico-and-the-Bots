3d77e42ce18fdf64e0b72c987b2a3148022f5c10
camero2734 <ckhaley21@gmail.com>
2024-06-29 15:37:29 +0200

    Check deployment issue

--- src/Helpers/get-badges.ts
+++ src/Helpers/get-badges.ts
@@ -33,39 +33,39 @@ export const badgeLoader = async (member: GuildMember, options: BadgeLoaderOptio
 async function* getBadge(member: GuildMember, numGolds: number, placeNum: number) {
     const ignore: string[] = [];
 
-    yield await createBadge("booster.png", async function () {
+    yield await createBadge("booster.png", async () => {
         return member.roles.cache.has("585527743324880897");
     });
 
-    yield await createBadge("staff.png", async function () {
+    yield await createBadge("staff.png", async () => {
         return member.roles.cache.has("330877657132564480");
     });
 
-    yield await createBadge("rich.png", async function () {
+    yield await createBadge("rich.png", async () => {
         return member.roles.cache.has("350036748404785153");
     });
 
-    yield await createBadge("firebreather.png", async function () {
+    yield await createBadge("firebreather.png", async () => {
         return member.roles.cache.has("283272728084086784");
     });
 
-    yield await createBadge("cliqueart.png", async function () {
+    yield await createBadge("cliqueart.png", async () => {
         return member.roles.cache.has("705224524098043914");
     });
 
-    yield await createBadge("youtube.png", async function () {
+    yield await createBadge("youtube.png", async () => {
         return member.roles.cache.has("341027502703116289");
     });
 
-    yield await createBadge("commonfren.png", async function () {
+    yield await createBadge("commonfren.png", async () => {
         return member.roles.cache.has("332021614256455690");
     });
 
-    yield await createBadge("artist.png", async function () {
+    yield await createBadge("artist.png", async () => {
         return member.roles.cache.has("341029793954922496");
     });
 
-    yield await createBadge("top10.png", async function () {
+    yield await createBadge("top10.png", async () => {
         if (placeNum <= 10) {
             ignore.push("top100.png", "top50.png", "top25.png");
             return true;
@@ -73,7 +73,7 @@ async function* getBadge(member: GuildMember, numGolds: number, placeNum: number
         return false;
     });
 
-    yield await createBadge("level100.png", async function () {
+    yield await createBadge("level100.png", async () => {
         if (member.roles.cache.has("449654945076215828")) {
             ignore.push("level50.png", "level25.png");
             return true;
@@ -81,7 +81,7 @@ async function* getBadge(member: GuildMember, numGolds: number, placeNum: number
         return false;
     });
 
-    yield await createBadge("top25.png", async function () {
+    yield await createBadge("top25.png", async () => {
         if (placeNum <= 25) {
             ignore.push("top100.png", "top50.png");
             return true;
@@ -89,7 +89,7 @@ async function* getBadge(member: GuildMember, numGolds: number, placeNum: number
         return false;
     });
 
-    yield await createBadge("top50.png", async function () {
+    yield await createBadge("top50.png", async () => {
         if (placeNum <= 50) {
             ignore.push("top100.png");
             return true;
@@ -97,60 +97,60 @@ async function* getBadge(member: GuildMember, numGolds: number, placeNum: number
         return false;
     });
 
-    yield await createBadge("ScavJumpsuit.png", async function () {
+    yield await createBadge("ScavJumpsuit.png", async () => {
         const badge = await prisma.badge.findUnique({
             where: { userId_type: { userId: member.id, type: "ScavJumpsuit" } }
         });
         return !!badge;
     });
 
-    yield await createBadge("ScavToplogo.png", async function () {
+    yield await createBadge("ScavToplogo.png", async () => {
         const badge = await prisma.badge.findUnique({
             where: { userId_type: { userId: member.id, type: "ScavToplogo" } }
         });
         return !!badge;
     });
 
-    yield await createBadge("ScavVulture.png", async function () {
+    yield await createBadge("ScavVulture.png", async () => {
         const badge = await prisma.badge.findUnique({
             where: { userId_type: { userId: member.id, type: "ScavVulture" } }
         });
         return !!badge;
     });
 
-    yield await createBadge("ScavHunt2019.png", async function () {
+    yield await createBadge("ScavHunt2019.png", async () => {
         const badge = await prisma.badge.findUnique({
             where: { userId_type: { userId: member.id, type: "ScavHunt2019" } }
         });
         return !!badge;
     });
 
-    yield await createBadge("AndreBlackWhite.png", async function () {
+    yield await createBadge("AndreBlackWhite.png", async () => {
         const badge = await prisma.badge.findUnique({ where: { userId_type: { userId: member.id, type: "ANDRE" } } });
         return !!badge;
     });
 
-    yield await createBadge("escapedDEMA.png", async function () {
+    yield await createBadge("escapedDEMA.png", async () => {
         const badge = await prisma.badge.findUnique({
             where: { userId_type: { userId: member.id, type: "ESCAPED_DEMA" } }
         });
         return !!badge;
     });
 
-    yield await createBadge("lgbt.png", async function () {
+    yield await createBadge("lgbt.png", async () => {
         const badge = await prisma.badge.findUnique({ where: { userId_type: { userId: member.id, type: "LGBT" } } });
         return !!badge;
     });
 
-    yield await createBadge("qotw.png", async function () {
+    yield await createBadge("qotw.png", async () => {
         return member.roles.cache.has(roles.qotwContributor);
     });
 
-    yield await createBadge("teamWinner.png", async function () {
+    yield await createBadge("teamWinner.png", async () => {
         return member.roles.cache.has("503645677574684683");
     });
 
-    yield await createBadge("level50.png", async function () {
+    yield await createBadge("level50.png", async () => {
         if (member.roles.cache.has("449654893108527114")) {
             ignore.push("level25.png");
             return true;
@@ -158,15 +158,15 @@ async function* getBadge(member: GuildMember, numGolds: number, placeNum: number
         return false;
     });
 
-    yield await createBadge("top100.png", async function () {
+    yield await createBadge("top100.png", async () => {
         return (placeNum <= 50);
     });
 
-    yield await createBadge("level25.png", async function () {
+    yield await createBadge("level25.png", async () => {
         return member.roles.cache.has("449654670357692416");
     });
 
-    yield await createBadge("gold100.png", async function () {
+    yield await createBadge("gold100.png", async () => {
         if (numGolds >= 100) {
             ignore.push("gold50.png", "gold25.png", "gold10.png", "gold5.png");
             return true;
@@ -174,7 +174,7 @@ async function* getBadge(member: GuildMember, numGolds: number, placeNum: number
         return false;
     });
 
-    yield await createBadge("gold50.png", async function () {
+    yield await createBadge("gold50.png", async () => {
         if (numGolds >= 50) {
             ignore.push("gold25.png", "gold10.png", "gold5.png");
             return true;
@@ -182,14 +182,14 @@ async function* getBadge(member: GuildMember, numGolds: number, placeNum: number
         return false;
     });
 
-    yield await createBadge("gold25.png", async function () {
+    yield await createBadge("gold25.png", async () => {
         if (numGolds >= 25) {
             ignore.push("gold10.png", "gold5.png");
             return true;
         }
         return false;
     });
-    yield await createBadge("gold10.png", async function () {
+    yield await createBadge("gold10.png", async () => {
         if (numGolds >= 10) {
             ignore.push("gold5.png");
             return true;
@@ -198,11 +198,11 @@ async function* getBadge(member: GuildMember, numGolds: number, placeNum: number
         return false;
     });
 
-    yield await createBadge("gold5.png", async function () {
+    yield await createBadge("gold5.png", async () => {
         return numGolds >= 5;
     });
 
-    yield await createBadge("dema.png", async function () {
+    yield await createBadge("dema.png", async () => {
         return member.roles.cache.has("451217741584793601");
     });
 
@@ -211,7 +211,7 @@ async function* getBadge(member: GuildMember, numGolds: number, placeNum: number
         yield await districtBadge(bishop.name, F.intColorToRGB(bishop.role.color));
     }
 
-    yield await createBadge("banditos.png", async function () {
+    yield await createBadge("banditos.png", async () => {
         return true;
     });
 

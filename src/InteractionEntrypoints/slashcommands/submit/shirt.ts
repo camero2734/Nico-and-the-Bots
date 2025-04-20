9acc722bd9972547bb355c16b57c74372d5cb44a
camero2734 <42698419+camero2734@users.noreply.github.com>
2025-04-20 17:55:15 +0200

    Send to staff chan:

--- src/InteractionEntrypoints/slashcommands/submit/shirt.ts
+++ src/InteractionEntrypoints/slashcommands/submit/shirt.ts
@@ -1,5 +1,5 @@
 import { EmbedBuilder } from "discord.js";
-import { roles } from "../../../Configuration/config";
+import { channelIDs, roles } from "../../../Configuration/config";
 import { SlashCommand } from "../../../Structures/EntrypointSlashCommand";
 
 const command = new SlashCommand({
@@ -16,21 +16,34 @@ command.setHandler(async (ctx) => {
         .setTitle("Shirt Discussion")
         .setDescription("Please reply (via the context menu action) to this message with your proposed announcement.")
         .setFooter({ text: "Thank you for your contribution!" });
-    const message = await dm.send({ embeds: [embed], components: [actionRow] });
+    const message = await dm.send({ embeds: [embed], components: [shirtReplyActionRow] });
 
     await ctx.editReply({
         content: `Please continue in [your DMs](${message.url})`,
     });
 });
 
-const actionRow = command.addReplyListener("shirtReply", async (reply, repliedTo) => {
-    console.log("Got reply", reply.content);
+const shirtReplyActionRow = command.addReplyListener("shirtReply", async (reply, repliedTo) => {
+    if (!reply.member) return;
+
+    const footer = new EmbedBuilder()
+        .setFooter({ text: reply.member?.displayName, iconURL: reply.member.avatarURL() || undefined });
+
+    const staffChan = await reply.guild?.channels.fetch(channelIDs.shirtSuggestions);
+    if (!staffChan || !staffChan.isSendable()) return;
+
+    await staffChan.send({
+        content: reply.content,
+        embeds: [footer],
+        files: reply.attachments.map((attachment) => attachment.url),
+        allowedMentions: { parse: [] },
+    });
+
     const embed = new EmbedBuilder()
-        .setDescription("Thank you for your contribution!")
-        .setColor("#FFFFFF")
+        .setDescription("Your message has been sent to the staff team.")
         .setFooter({ text: "Thank you for your contribution!" });
 
-    await repliedTo.edit({ embeds: [embed] });
+    await repliedTo.edit({ embeds: [embed], components: [] });
 });
 
 export default command;

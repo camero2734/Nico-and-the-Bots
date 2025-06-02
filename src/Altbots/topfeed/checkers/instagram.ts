3bc346298decc7171d960e7bde30668f29924652
camero2734 <42698419+camero2734@users.noreply.github.com>
2025-06-02 10:57:25 +0200

    Fetch post count from /embed endpoint instead of discord, unless that fails

--- src/Altbots/topfeed/checkers/instagram.ts
+++ src/Altbots/topfeed/checkers/instagram.ts
@@ -53,7 +53,7 @@ async function initializeInstagram() {
   initialized = true;
 }
 
-async function fetchOpengraphData(user: string) {
+async function fetchOpengraphDataBackup(user: string) {
   const testChan = await topfeedBot.guild.channels.fetch(channelIDs.bottest);
   if (!testChan || !testChan.isSendable()) throw new Error("Test channel not found or is not text-based");
 
@@ -76,6 +76,49 @@ async function fetchOpengraphData(user: string) {
     throw new Error(`No post count found in OpenGraph description for ${user}`);
   }
   return Number.parseInt(postCountMatch[1], 10);
+}
+
+async function fetchOpengraphData(user: string): Promise<number> {
+  try {
+    const data = await fetch(`https://www.instagram.com/${user}/embed`, {
+      "credentials": "omit",
+      "headers": {
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:138.0) Gecko/20100101 Firefox/138.0",
+          "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+          "Accept-Language": "en-US,en;q=0.5",
+          "Sec-GPC": "1",
+          "Alt-Used": "www.instagram.com",
+          "Upgrade-Insecure-Requests": "1",
+          "Sec-Fetch-Dest": "iframe",
+          "Sec-Fetch-Mode": "navigate",
+          "Sec-Fetch-Site": "cross-site",
+          "Priority": "u=6"
+      },
+      "referrer": "https://www.discord.com/",
+      "method": "GET",
+      "mode": "cors"
+    });
+
+    const text = await data.text();
+
+    const match = text.split('posts_count\\":')[1].split(',')[0];
+    console.log(match);
+    if (!match) {
+      throw new Error("Failed to parse the number of posts from the response.");
+    }
+    const postCount = parseInt(match, 10);
+    if (isNaN(postCount)) {
+      throw new Error("Parsed post count is not a valid number.");
+    }
+    return postCount;
+  } catch (error) {
+    const testChan = await topfeedBot.guild.channels.fetch(channelIDs.bottest);
+    if (!testChan || !testChan.isTextBased()) throw new Error("Test channel not found or is not text-based");
+    const message = error instanceof Error ? error.message : 'Unknown error fetching Instagram opengraph data';
+    console.error(`Error fetching Instagram opengraph data for ${user}:`, error);
+    await testChan.send(`${userMention(userIDs.me)} Error fetching Instagram opengraph data for ${user}: ${message}, trying backup method...`);
+    return fetchOpengraphDataBackup(user);
+  }
 }
 
 // Extract the best media URL from a post or carousel item

5e1f1ca97bffd1870768806e0b4b44262ad75c66
camero2734 <42698419+camero2734@users.noreply.github.com>
2025-06-03 23:30:03 +0200

    Add command to fetch audit logs to see who had bfx roles

---
+++ src/InteractionEntrypoints/slashcommands/superstaff/auditlogs.ts
@@ -0,0 +1,63 @@
+import { AttachmentBuilder, AuditLogEvent } from "discord.js";
+import { userIDs } from "../../../Configuration/config";
+import { CommandError } from "../../../Configuration/definitions";
+import { SlashCommand } from "../../../Structures/EntrypointSlashCommand";
+
+const command = new SlashCommand({
+  description: "Audit logs",
+  options: [],
+});
+
+command.setHandler(async (ctx) => {
+  await ctx.deferReply();
+
+  if (ctx.user.id !== userIDs.me) throw new CommandError("You cannot use this command");
+
+  // 26 May 2025
+  const date = new Date("2025-05-26T00:00:00Z");
+
+  const usersWithRole = [];
+
+  let lastId: string | undefined;
+
+  outer: for (let i = 0; i < 100; i++) {
+    console.log(`Fetching audit logs, iteration ${i + 1}`);
+    const logs = await ctx.guild.fetchAuditLogs({
+      type: AuditLogEvent.MemberRoleUpdate,
+      limit: 100,
+      before: lastId,
+    });
+
+    if (logs.entries.size === 0) break;
+
+    for (const entry of logs.entries.values()) {
+      if (entry.createdAt < date) break outer;
+
+      if (
+        entry.changes.some(
+          (change) => change.key === "$add" && change.new?.map((x) => x.id).includes("1373674037204484167"),
+        )
+      ) {
+        usersWithRole.push(entry.targetId);
+      }
+
+      if (
+        entry.changes.some(
+          (change) => change.key === "$add" && change.new?.map((x) => x.id).includes("1373724238695108761"),
+        )
+      ) {
+        usersWithRole.push(entry.targetId);
+      }
+    }
+  }
+
+  const txtFile = Buffer.from(usersWithRole.join("\n"), "utf-8");
+  const fileName = `users-with-role-${Date.now().toString()}.txt`;
+
+  await ctx.editReply({
+    content: `Found ${usersWithRole.length} users with the role.`,
+    files: [new AttachmentBuilder(txtFile, { name: fileName })],
+  });
+});
+
+export default command;

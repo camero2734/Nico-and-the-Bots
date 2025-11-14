e17a07d9883ce42e1f04cb4eb904958d95d4ca81
camero2734 <42698419+camero2734@users.noreply.github.com>
2025-11-14 20:03:34 +0100

    Debug

--- src/InteractionEntrypoints/slashcommands/staff/edit/firebreathers.ts
+++ src/InteractionEntrypoints/slashcommands/staff/edit/firebreathers.ts
@@ -59,7 +59,7 @@ command.setHandler(async (ctx) => {
     }
 
     await ctx.editReply({
-      content: "User can no longer apply for firebreathers.",
+      content: `User can no longer apply for firebreathers.\n\`\`\`json\n${JSON.stringify(latestApplication)}\n\`\`\``,
     });
   } else if (action === "RESET_TIMER") {
     // Find the most recent firebreathers application and reset the timer

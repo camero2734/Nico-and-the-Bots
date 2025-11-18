440f5a431228f7607c3c8fc2f1658ae516a4ee03
camero2734 <42698419+camero2734@users.noreply.github.com>
2025-11-18 23:17:38 +0100

    Debug

--- src/InteractionEntrypoints/slashcommands/superstaff/migratecolorroles/up.ts
+++ src/InteractionEntrypoints/slashcommands/superstaff/migratecolorroles/up.ts
@@ -90,11 +90,14 @@ async function calculateRoleChanges(ctx: typeof command.ContextType) {
       }
       case "noChange": {
         const role = ctx.guild.roles.cache.find((r) => r.name.toLowerCase() === change.name.toLowerCase());
+
+        const data = {
+          availableRoles: ctx.guild.roles.cache.map((r) => r.name),
+          targetRole: change.name,
+        };
+        await Bun.file("role-migration-debug.txt").write(JSON.stringify(data, null, 2));
+
         if (!role) {
-          console.log(
-            "Available roles",
-            ctx.guild.roles.cache.map((r) => r.name),
-          );
           throw new Error(`Role for noChange not found: ${change.name}`);
         }
         if (role.hexColor.toLowerCase() !== (change.expectedColor.primaryColor as string).toLowerCase()) {

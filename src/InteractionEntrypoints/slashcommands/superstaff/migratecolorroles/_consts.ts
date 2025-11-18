753c10690cd996699d066299553f1ededd5babad
camero2734 <42698419+camero2734@users.noreply.github.com>
2025-11-18 21:57:33 +0100

    Edit migratecolorroles for new roles

--- src/InteractionEntrypoints/slashcommands/superstaff/migratecolorroles/_consts.ts
+++ src/InteractionEntrypoints/slashcommands/superstaff/migratecolorroles/_consts.ts
@@ -1,31 +1,119 @@
-import type { ColorResolvable, Snowflake } from "discord.js";
-import { roles } from "../../../../Configuration/config";
-import F from "../../../../Helpers/funcs";
+import type { ColorResolvable, RoleColorsResolvable } from "discord.js";
 
-export interface Change {
-  from: ColorResolvable;
-  to: ColorResolvable;
-}
-
-const colors = {
-  ...roles.colors.tier1,
-  ...roles.colors.tier2,
-  ...roles.colors.tier3,
-  ...roles.colors.tier4,
-  ...roles.colors.DExclusive,
+type DeleteRole = {
+  type: "delete";
+  name: string;
 };
+const DeleteRole = (name: string): DeleteRole => ({ type: "delete", name });
 
-const _changes: Partial<Record<keyof typeof colors, Change>> = {
-  "Rebel Red": { from: "#ff0060", to: "#D30E3B" },
-  "The Red and Go": { from: "#e86a6e", to: "#E74F54" },
-  "Pink You Out": { from: "#ff1493", to: "#EEB8DD" },
-  "Kitchen Pink": { from: "#ff9dae", to: "#C54B8C" },
-  "Ned Blue": { from: "#c6e2ff", to: "#008FA2" },
-  "Regional at Blue": { from: "#aebfd8", to: "#6485A9" },
-  "Pet Cheetah Purple": { from: "#7113bd", to: "#BB80FF" },
+type RecolorRole = {
+  type: "changeColor";
+  name: string;
+  to: RoleColorsResolvable;
 };
+const RecolorRole = (name: string, to: ColorResolvable): RecolorRole => ({
+  type: "changeColor",
+  name,
+  to: { primaryColor: to },
+});
 
-export const changes = Object.fromEntries(F.entries(_changes).map(([key, value]) => [colors[key], value])) as Record<
-  Snowflake,
-  Change
->;
+type RenameRole = {
+  type: "rename";
+  from: string;
+  to: string;
+  expectedColor: RoleColorsResolvable;
+};
+const RenameRole = (from: string, to: string, expectedColor: ColorResolvable): RenameRole => ({
+  type: "rename",
+  from,
+  to,
+  expectedColor: { primaryColor: expectedColor },
+});
+
+type RenameAndRecolorRole = {
+  type: "changeAndRename";
+  from: string;
+  to: string;
+  colorTo: RoleColorsResolvable;
+};
+const RenameAndRecolorRole = (from: string, to: string, colorTo: ColorResolvable): RenameAndRecolorRole => ({
+  type: "changeAndRename",
+  from,
+  to,
+  colorTo: { primaryColor: colorTo },
+});
+
+type AddRole = {
+  type: "add";
+  name: string;
+  color: ColorResolvable;
+};
+const AddRole = (name: string, color: ColorResolvable): AddRole => ({ type: "add", name, color });
+
+type NoChange = {
+  type: "noChange";
+  name: string;
+  expectedColor: RoleColorsResolvable;
+};
+const NoChange = (name: string, expectedColor: ColorResolvable): NoChange => ({
+  type: "noChange",
+  name,
+  expectedColor: { primaryColor: expectedColor },
+});
+
+export type Change = DeleteRole | RecolorRole | RenameRole | RenameAndRecolorRole | AddRole | NoChange;
+
+export const changes: Array<Change> = [
+  AddRole("Breach Peach", "#ffceb1"),
+  NoChange("No Pink Intended", "#b18f95"),
+  NoChange("Goldies Station", "#a38a00"),
+  RenameAndRecolorRole("Bandito Green", "RAWFERN", "#8a9a5b"),
+  RenameAndRecolorRole("Cheetah Tan", "Garbeige", "#ab9d85"),
+  RenameAndRecolorRole("Vulture Brown", "Brownstairs", "#915919"),
+  NoChange("Regional at Blue", "#6485a9"),
+
+  DeleteRole("Trapdoorange"),
+  RenameRole("Lavaish", "Trapdoororange", "#ff6c0e"),
+  AddRole("Pear in My Heart", "#d1e231"),
+  RenameAndRecolorRole("Jumpsuit Green", "Center Moss", "#63b76c"),
+  NoChange("Holding on to Blue", "#4a83e6"),
+  NoChange("At the Risk of Feeling Plum", "#673147"),
+  RenameRole("Dema Gray", "City Walls Gray", "#9b9bad"),
+  RenameRole("Pink You Out", "Rosebot Voices", "#eeb8dd"),
+  NoChange("Kitchen Pink", "#c54b8c"),
+  NoChange("Mulberry Jam", "#b00b69"),
+  AddRole("Ruby", "#b8000f"),
+  RenameRole("Maroontines in the Night", "My Blood", "#800000"),
+  NoChange("Fairly Lilac", "#ccacea"),
+  RenameAndRecolorRole("Pet Cheetah Purple", "Redecorgrape", "#9966cc"),
+  RenameRole("Pantaloon Purple", "Quiet is Violet", "#8f81ff"),
+  NoChange("Navygating", "#0c0caa"),
+  DeleteRole("Never Teal It"),
+  RenameRole("Ned Blue", "Never Teal It", "#008fa2"),
+  NoChange("Forest Green", "#11806a"),
+
+  RenameRole("Paladin Pink", "Bubblegum for Hands", "#ff7d9a"),
+  NoChange("Rebel Red", "#d30e3b"),
+  AddRole("Glowing Eyes", "#ecfe2a"),
+  RenameRole("Trees Green", "Chlorine Green", "#a9ff9f"),
+  RenameAndRecolorRole("March to the Cyan", "March to the Seafoam", "#9fe2bf"),
+  NoChange("Formidablue", "#81ccdd"),
+  NoChange("Midwest Indigo", "#4b00b2"),
+  DeleteRole("Ode to Pink"),
+  AddRole("Cottonwood Candy", "#ffadbf"),
+  RenameAndRecolorRole("The Red and Go", "Coral Radio", "#ff5c5c"),
+  DeleteRole("Ochrecompensate"),
+  DeleteRole("Torchbearer Orange"),
+  AddRole("Rust Around the Rim", "#d97448"),
+  AddRole("Heatless Fire", "#ffa52c"),
+  NoChange("Bandito Yellow", "#fce300"),
+  AddRole("The Lime", "#53ee5b"),
+  NoChange("Oh Mint Believer", "#c4fccc"),
+  RenameRole("Silver Screen", "Heavydirtystone", "#bec2cb"),
+
+  NoChange("CLANCY BLACK", "#000001"),
+  NoChange("DMAORG WHITE", "#ffffff"),
+
+  AddRole("The Contrast", "#e0115f"),
+  AddRole("Sky Away", "#00bfff"),
+];
--- src/InteractionEntrypoints/slashcommands/superstaff/migratecolorroles/up.ts
+++ src/InteractionEntrypoints/slashcommands/superstaff/migratecolorroles/up.ts
@@ -1,21 +1,129 @@
-import type { Snowflake } from "discord.js";
 import { SlashCommand } from "../../../../Structures/EntrypointSlashCommand";
-import { type Change, changes } from "./_consts";
+import { changes } from "./_consts";
+import { roles, userIDs } from "Configuration/config";
+import { ApplicationCommandOptionType } from "discord.js";
+import { EmbedBuilder } from "discord.js";
 
 const command = new SlashCommand({
   description: "Makes changes",
-  options: [],
+  options: [
+    {
+      name: "actual",
+      description: "Actually run the migration; otherwise it's a dry run",
+      required: false,
+      type: ApplicationCommandOptionType.Boolean,
+    },
+  ],
 });
 
 command.setHandler(async (ctx) => {
   await ctx.deferReply();
-  for (const [roleId, change] of Object.entries(changes) as [Snowflake, Change][]) {
-    const role = await ctx.guild.roles.fetch(roleId);
-    if (!role) continue;
 
-    await role.edit({ color: change.to });
+  // const { actual } = ctx.opts;
+
+  if (ctx.user.id !== userIDs.me) {
+    return await ctx.editReply("You do not have permission to use this command.");
   }
-  await ctx.editReply("Done");
+
+  const colorRoles = Object.values(roles.colors).flatMap((x) => Object.values(x));
+  const existingRoles = new Set<string>();
+  const rolesAfterChange = new Set<string>();
+  for (const roleId of colorRoles) {
+    const role = ctx.guild.roles.cache.get(roleId);
+    if (!role) throw new Error(`Role with ID ${roleId} not found in guild`);
+    existingRoles.add(role.name.toLowerCase());
+    rolesAfterChange.add(role.name.toLowerCase());
+  }
+
+  for (const change of changes) {
+    switch (change.type) {
+      case "add": {
+        // const role = await ctx.guild.roles.create({
+        //   name: change.name,
+        //   color: change.color,
+        //   reason: "Migrating color roles",
+        // });
+        rolesAfterChange.add(change.name.toLowerCase());
+        break;
+      }
+      case "delete": {
+        const role = ctx.guild.roles.cache.find((r) => r.name.toLowerCase() === change.name.toLowerCase());
+        if (!role) throw new Error(`Role to delete not found: ${change.name}`);
+
+        existingRoles.delete(change.name.toLowerCase());
+        rolesAfterChange.delete(change.name.toLowerCase());
+        // if (role) {
+        //   await role.delete("Migrating color roles");
+        // }
+        break;
+      }
+      case "changeColor": {
+        const role = ctx.guild.roles.cache.find((r) => r.name.toLowerCase() === change.name.toLowerCase());
+        if (!role) throw new Error(`Role to change color not found: ${change.name}`);
+        existingRoles.delete(change.name.toLowerCase());
+        // if (role) {
+        //   await role.setColor(change.to, "Migrating color roles");
+        // }
+        break;
+      }
+      case "rename": {
+        const role = ctx.guild.roles.cache.find((r) => r.name.toLowerCase() === change.from.toLowerCase());
+        if (!role) throw new Error(`Role to rename not found: ${change.from}`);
+
+        if (role.hexColor.toLowerCase() !== (change.expectedColor.primaryColor as string).toLowerCase()) {
+          throw new Error(
+            `Role color mismatch for ${change.from}: expected ${change.expectedColor.primaryColor}, got ${role.hexColor}`,
+          );
+        }
+
+        rolesAfterChange.delete(change.from.toLowerCase());
+        rolesAfterChange.add(change.to.toLowerCase());
+        existingRoles.delete(change.from.toLowerCase());
+        // if (role) {
+        //   await role.setName(change.to, "Migrating color roles");
+        // }
+        break;
+      }
+      case "changeAndRename": {
+        const role = ctx.guild.roles.cache.find((r) => r.name.toLowerCase() === change.from.toLowerCase());
+        if (!role) throw new Error(`Role to change and rename not found: ${change.from}`);
+        rolesAfterChange.delete(change.from.toLowerCase());
+        rolesAfterChange.add(change.to.toLowerCase());
+        existingRoles.delete(change.from.toLowerCase());
+        // if (role) {
+        //   await role.setName(change.to, "Migrating color roles");
+        //   await role.setColors(change.colorTo, "Migrating color roles");
+        // }
+        break;
+      }
+      case "noChange": {
+        const role = ctx.guild.roles.cache.find((r) => r.name.toLowerCase() === change.name.toLowerCase());
+        if (!role) throw new Error(`Role for noChange not found: ${change.name}`);
+        if (role.hexColor.toLowerCase() !== (change.expectedColor.primaryColor as string).toLowerCase()) {
+          throw new Error(
+            `Role color mismatch for ${change.name}: expected ${change.expectedColor.primaryColor}, got ${role.hexColor}`,
+          );
+        }
+
+        existingRoles.delete(change.name.toLowerCase());
+
+        break;
+      }
+      default: {
+        throw new Error(`Unhandled change type: ${JSON.stringify(change satisfies never)}`);
+      }
+    }
+  }
+
+  if (existingRoles.size > 0) {
+    await ctx.editReply(
+      `ERROR! Roles that were expected to be changed but were not modified: ${[...existingRoles].join(", ")}`,
+    );
+    return;
+  }
+
+  const embed = new EmbedBuilder().setTitle("Roles after migration").setDescription([...rolesAfterChange].join("\n"));
+  return await ctx.editReply({ embeds: [embed] });
 });
 
 export default command;

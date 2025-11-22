b1503b63324b44dc1649f78567a43446528d0647
camero2734 <42698419+camero2734@users.noreply.github.com>
2025-11-22 16:43:36 +0100

    Make datasource url optional

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: env("DATABASE_URL"),
+    url: process.env.DATABASE_URL as string,
   },
 });

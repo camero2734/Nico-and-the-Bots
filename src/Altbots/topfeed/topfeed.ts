a8ca95379076f92c139af8e52645a8474972d298
camero2734 <42698419+camero2734@users.noreply.github.com>
2026-03-30 21:19:31 +0200

    Fix init order

--- src/Altbots/topfeed/topfeed.ts
+++ src/Altbots/topfeed/topfeed.ts
@@ -1,7 +1,6 @@
 import { Client, Events } from "discord.js";
 import { guildID } from "../../Configuration/config";
 import secrets from "../../Configuration/secrets";
-import { queue } from "../../Helpers/jobs";
 
 const client = new Client({
   intents: [
@@ -22,9 +21,5 @@ const client = new Client({
 
 await client.login(secrets.bots.keons);
 await new Promise((resolve) => client.once(Events.ClientReady, resolve));
-
-for (const jobName of ["TWITTER", "INSTAGRAM", "YOUTUBE", "WEBSITES"] as const) {
-  queue.topfeed.add({ type: jobName }, { repeat: { every: 1000 * 60 * 5 }, deduplication: { id: jobName } });
-}
 
 export const keonsGuild = await client.guilds.fetch(guildID);
--- src/Helpers/jobs/index.ts
+++ src/Helpers/jobs/index.ts
@@ -9,4 +9,8 @@ export const jobs = {
 
 const queue = createQueueClient<typeof jobs>();
 
+for (const jobName of ["TWITTER", "INSTAGRAM", "YOUTUBE", "WEBSITES"] as const) {
+  queue.topfeed.add({ type: jobName }, { repeat: { every: 1000 * 60 * 5 }, deduplication: { id: jobName } });
+}
+
 export { queue };
